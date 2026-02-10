9b010f3cc5b3abddc8608a7e468ca7e80d476b2a
mqmdog <3374382168@qq.com>
2026-02-10 18:09:51 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+import { createHtmlPlugin } from "vite-plugin-html";
+
+
+const getViteEnv = (mode, target) => {
+  return loadEnv(mode, process.cwd())[target];
+};
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-    VueSetupExtend(),
+export default ({ mode }) => { 
+ return  defineConfig({
+    plugins: [
+      vue(),
+      VueDevTools(),
+      VueSetupExtend(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: getViteEnv(mode, "VITE_APP_TITLE"),
+          },
+        },
+    }),
   ],
-  resolve: {
-    alias: {
+    resolve: {
+      alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+
+}
